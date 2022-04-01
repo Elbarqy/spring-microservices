4baf8d079fd273e062d0565fa2f62ec032548522
Elbarqy <elbarky.dev@gmail.com>
2022-04-01 18:46:37 +0200

    first commit

---
+++ product service/product-service/src/test/java/com/elbarqy/appstore/product/service/ProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elbarqy.appstore.product.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
